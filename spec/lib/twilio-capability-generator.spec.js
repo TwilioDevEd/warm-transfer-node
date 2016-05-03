8dde71bd7e1f177049791718d27ef304249006eb
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-05-03 13:34:03 -0300

    Add twilio capability generator

---
+++ spec/lib/twilio-capability-generator.spec.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect
+  , app = require('../../app.js')
+  , twilioCapabilityGenerator = require('../../lib/twilio-capability-generator')
+  , cheerio = require('cheerio');
+
+describe('twilio-capability-generator', function () {
+
+  describe('#generate', function () {
+    it('responds with generated token', function () {
+      var token = twilioCapabilityGenerator('agentId');
+      expect(token).to.not.be.null;
+    });
+  });
+
+});
