3d24f725a1e5ee93345d501f8b80fd3756bf3251
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-05-05 14:14:26 -0300

    Trace msg on error

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    console.trace(err);
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
